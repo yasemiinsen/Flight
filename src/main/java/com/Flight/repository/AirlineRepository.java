c02c76547d91dbcc7ce6dae796e45d9ffe78300b
yasemin <yasemin10085@gmail.com>
2025-03-17 13:40:22 +0300

    mapper repo services are changed

---
+++ src/main/java/com/Flight/repository/AirlineRepository.java
@@ -0,0 +1,9 @@
+package com.Flight.repository;
+
+import com.Flight.entity.Airline;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirlineRepository extends JpaRepository<Airline, Integer> {
+}
